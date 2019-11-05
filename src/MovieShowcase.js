1cd79de7361812894a6bc2aaf1b2500c1884f5c6
Lyn Muldrow <lyn.muldrow@flatironschool.com>
2019-11-05 20:53:01 +0000

    Automatically backed up by Learn

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -5,7 +5,7 @@ import movieData from './data.js'
 export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
-    data.map()
+    movieData.map(item => <li>{item}</li>)
   }
 
   render() {
